9d467511d04c9bfd44879a8a61436b03584a9b98
Adil-im <Adhilimam92@gmail.com>
2025-08-19 20:24:00 +0530

    Initial Commit :)

---
+++ polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Kill all running polybar instances
+killall -q polybar
+
+# Wait until polybar has shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch your bar (replace 'example' if needed)
+polybar example & 
+
+echo "polybar launching..."
+
